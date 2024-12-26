85ef03290db0c17537919b2b6172f8b6b8dd1386
information278 <gretcenkovladislav5@gmail.com>
2024-12-26 18:36:22 +0300

    выполнил 7 лабу

--- golang/main.go
+++ golang/main.go
@@ -5,10 +5,12 @@ import (
 
 	lab4 "isuct.ru/informatics2022/lab4"
 	lab6 "isuct.ru/informatics2022/lab6"
+	lab7 "isuct.ru/informatics2022/lab7"
 )
 
 func main() {
 	fmt.Println("Гретченко Владислав Игоревич")
 	lab4.Runlab4()
 	lab6.Runlab6()
+	lab7.Runlab7()
 }
